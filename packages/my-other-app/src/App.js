bc62e7a11454a71244ee5cb567c07ca868d37206
Markus Coetzee <mks.coetzee@gmail.com>
2024-02-22 14:34:56 +0200

    Trigger deploy

--- packages/my-other-app/src/App.js
+++ packages/my-other-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>It has been switched! 01</h1>
-        <p>With PR comment</p>
+        <p>With PR comment - final</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
