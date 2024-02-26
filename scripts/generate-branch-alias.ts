2a23ccbc0e489c1bef454f91d9c0d49853ced195
Markus Coetzee <mks.coetzee@gmail.com>
2024-02-26 11:06:29 +0200

    Fix branch alias script (#11)
    * Remove return type
    
    * Fix script

--- scripts/generate-branch-alias.ts
+++ scripts/generate-branch-alias.ts
@@ -14,16 +14,15 @@ if (!branch || !project) {
 setOutput('alias', generateBranchAliasUrl(branch, project))
 
 function generateBranchAliasUrl(branch: string, project: string) {
-  let sanitisedBranch = trimFromEdges(branch.toLowerCase().replace(INVALID_CHARS_REGEX, '-'), '-');
-
-  if (sanitisedBranch.length > MAX_ALIAS_LENGTH) {
-    sanitisedBranch = sanitisedBranch.substring(0, MAX_ALIAS_LENGTH);
-  }
+  const sanitisedBranch = trimFromEdges(
+    branch.toLowerCase().replace(INVALID_CHARS_REGEX, '-').slice(0, MAX_ALIAS_LENGTH),
+    '-'
+  );
 
   return sanitisedBranch ? `https://${sanitisedBranch}.${project}.pages.dev` : '';
 }
 
-function trimFromEdges(str: string, char: string): string {
+function trimFromEdges(str: string, char: string) {
   while (str.charAt(0) === char) {
     if (str.length === 1) {
       return '';
